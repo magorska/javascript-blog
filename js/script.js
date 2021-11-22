3b682fd6a0f1549cc1b13aece13abea499502d4b
Marta Gorska <magorska.95@gmail.com>
2021-11-22 12:22:23 +0100

    Add active class

--- js/script.js
+++ js/script.js
@@ -1,29 +1,30 @@
 'use strict';
 
 function titleClickHandler(event){
+  const clickedElement = this;
   console.log('Link was clicked!');
-  console.log(event);
 
-  /* remove class 'active' from all article links  */
+  /* [DONE] remove class 'active' from all article links  */
 
   const activeLinks = document.querySelectorAll('.titles a.active');
 
-    for(let activeLink of activeLinks){
+  for(let activeLink of activeLinks){
     activeLink.classList.remove('active');
-    }
+  }
 
-  /* add class 'active' to the clicked link */
+  /* [IN PROGRESS] add class 'active' to the clicked link */
 
-  /* remove class 'active' from all articles */
+  console.log('clickedElement:', clickedElement);
+
+  clickedElement.classList.add('active');
+
+  /* [DONE] remove class 'active' from all articles */
 
   const activeArticles = document.querySelectorAll('.posts .active');
 
-    for(let activeArticle of activeArticle){
-    activeArticles.classList.remove('active');
+  for(let activeArticle of activeArticles){
+    activeArticle.classList.remove('active');
     }
-
-  
-
 
   /* get 'href' attribute from the clicked link */
 
