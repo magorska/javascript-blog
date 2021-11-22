6160131ad9ded30acd876f31b8409ebcafa84967
Marta Gorska <magorska.95@gmail.com>
2021-11-22 14:19:33 +0100

    Complite generateTitleLinks and debug

--- js/script.js
+++ js/script.js
@@ -42,8 +42,50 @@ function titleClickHandler(event){
   targetArticle.classList.add('active');
 }
 
-const links = document.querySelectorAll('.titles a');
 
-for(let link of links){
-  link.addEventListener('click', titleClickHandler);
-}
+// exercise 5.4
+
+const optArticleSelector = '.post',
+  optTitleSelector = '.post-title',
+  optTitleListSelector = '.titles';
+
+  function generateTitleLinks() {
+      
+    /* remove contents of titleList */
+    const titleList = document.querySelector(optTitleListSelector);
+    titleList.innerHTML = "";
+
+    /* find all the articles and save them to variable: articles */
+    const articles = document.querySelectorAll(optArticleSelector);
+    let html = "";
+
+    /* for each article */
+    for (let article of articles) {
+
+      /* get the article id */
+      const articleId = article.getAttribute("id");
+
+      /* find the title element */
+      const articleTitle = article.querySelector(optTitleSelector).innerHTML;
+
+      /* create HTML of the link */
+      const linkHTML =
+        '<li><a href="#"' + articleId + '><span>' + articleTitle + '</span></a></li>';
+      console.log(linkHTML);
+
+      /* insert link into titleList */
+      html = html + linkHTML;
+      console.log(html);
+    }
+
+    titleList.innerHTML = html;
+
+    const links = document.querySelectorAll(".titles a");
+    for (let link of links) {
+      link.addEventListener("click", titleClickHandler);
+    }
+  }
+
+
+generateTitleLinks();
+
